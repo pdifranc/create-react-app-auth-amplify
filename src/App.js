0c4ff22db45081d079ae328f4dfd10eef1419790
Paolo Di Francesco <frpaolo@amazon.com>
2020-03-26 14:34:31 +0100

    Add code to talk to the backend

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { Auth, API } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  post = async () => {
+    console.log('calling api');
+    const response = await API.post('tickets', '/items', {
+      body: {
+        id: '1',
+        name: 'hello amplify!'
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  };
+
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('tickets', '/tickets/');
+    alert(JSON.stringify(response, null, 2));
+  };
+
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('tickets', '/tickets/');
+    alert(JSON.stringify(response, null, 2));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Second deployment. from feature-branch-third
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p> Pick a file</p>
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
       </div>
     );
   }
